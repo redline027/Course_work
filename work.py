f50d59f5e5606781eb764f53f5d1224c178c9f2e
redline027 <floopi027@gmail.com>
2015-04-22 23:10:16 +0300

    New version

--- work.py
+++ work.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageTk
 from sympy import sqrt
 from lmfit import minimize, Parameters, Parameter, fit_report
 
-#Undistortion
+#Camera parameters
 fc = np.array([313.185205687689290, 312.634348462007840])
 cc = np.array([217.924273865532680, 127.719511013538310])
 kc = np.array([0.120594447004112, -0.211027111434246, 0.002419877275468, -0.002228812901654, 0.000000000000000])
@@ -18,44 +18,36 @@ cameraMatrix = np.array([[fc[0], 0, cc[0]],
                          [0, fc[1], cc[1]],
                          [0, 0, 1]])
 
-
+#Undistortion
 def undistor(img):
     map1, map2 = cv2.initUndistortRectifyMap(cameraMatrix, kc, R, cameraMatrix, (nx, ny), cv2.CV_32FC1)
     undist = cv2.remap(img, map1, map2, cv2.INTER_LINEAR)
     return undist
 
-f = open('results/test1.txt', 'w')
+#Files
+f = open('results/test9.txt', 'w')
+f_error = open('results/test9_error.txt', 'w')
+f_input = open('results/test9_input.txt', 'r')
 
 #get first frame
-cap = cv2.VideoCapture('video/test1.avi')
+cap = cv2.VideoCapture('video/test9.avi')
 ret, first_frame = cap.read()
 first_frame = undistor(first_frame)
-cv2.imwrite('first_frame.jpg', first_frame)
 
-#coordinates
-window = Tk()
-image = Image.open("first_frame.jpg")
-photo = ImageTk.PhotoImage(image)
-label = Label(image=photo)
-label.image = photo
-label.pack()
-
-def getXY(event):
-    src.append([float(event.x-2), float(event.y-2)])
-
+#read coordinates from file
 src = []
-window.bind('<ButtonPress>', getXY)
-window.mainloop()
+for i in xrange(4):
+    src.append([float(f_input.readline()), float(f_input.readline())])
 
 #Homography matrix
-cx = 12.0
-cy = 6.0
+cx = 8.0
+cy = 5.0
 src = np.array(src)
 dst = np.array([[0.0, 0.0], [cx, 0.0], [cx, cy], [0.0, cy]])
 ret = cv2.findHomography(dst, src)
 H = ret[0]
 
-#chess points
+#coordinates chess points
 points = []
 for i in range(int(cx)+1):
     for j in range(int(cy)+1):
@@ -77,7 +69,7 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
 cv2.cornerSubPix(first_gray, cor, (3, 3), (-1, -1), criteria)
 
 # coordinates of chessboard: template -> image(with given angle and translation)
-def F(angle, translation):
+def transformation(angle, translation):
     rotation_matrix, jac = cv2.Rodrigues(angle)
     transform = np.mat([[rotation_matrix[0, 0], rotation_matrix[0, 1], rotation_matrix[0, 2], translation[0]],
                         [rotation_matrix[1, 0], rotation_matrix[1, 1], rotation_matrix[1, 2], translation[1]],
@@ -94,7 +86,10 @@ def F(angle, translation):
             coordinates.append(c[1, 0]/c[2, 0])
     return np.array(coordinates)
 
+
 # define objective function: returns the array to be minimized
+# params - class Parameters from lmfit
+# data - coordinates that should be
 def residual(params, data):
     vals = params.valuesdict()
     angle_fit = np.zeros((3, 1))
@@ -105,7 +100,7 @@ def residual(params, data):
     translation_fit[0] = vals['translation0']
     translation_fit[1] = vals['translation1']
     translation_fit[2] = vals['translation2']
-    model = F(angle_fit, translation_fit)
+    model = transformation(angle_fit, translation_fit)
     return model - data
 
 # calculate position, H - homography matrix, coordinates - coordinates of points that we know
@@ -125,6 +120,7 @@ def position(H, coordinates):
     R1 = np.array([[h1[0], h2[0], h3[0]],
                   [h1[1], h2[1], h3[1]],
                   [h1[2], h2[2], h3[2]]], np.float64)
+
     ang, jac = cv2.Rodrigues(R1)
 
     # create data to be fitted
@@ -148,14 +144,17 @@ def position(H, coordinates):
 
     return params
 
+
+# write in file
 def write(params, cor, f):
     data = []
     for i in range(len(cor)):
         data.append(cor[i][0, 0])
         data.append(cor[i][0, 1])
     data = np.array(data)
-    f.write(str(params['translation0'].value) + ' ' + str(params['translation1'].value) + ' ' + str(params['translation2'].value) + ' ')
-    f.write('Mean error:' + str(np.mean(residual(params, data))) + '\n')
+    f.write(str(params['translation0'].value) + ' ' + str(params['translation1'].value) + ' ' + str(params['translation2'].value) + '\n')
+    f_error.write(str(np.mean(abs(residual(params, data)))) + '\n')
+
 
 # first position
 p = position(H, cor)
@@ -202,12 +201,14 @@ while(1):
 
     # Now update the previous frame and previous points
     old_gray = frame_gray.copy()
-    cor = good_new.reshape(-1,1,2)
+    cor = good_new.reshape(-1, 1, 2)
 
-    #Write camera position
+    # Write camera position
     p = position(H, cor)
     write(p, cor, f)
 
 f.close()
+f_error.close()
+f_input.close()
 cv2.destroyAllWindows()
 cap.release()
